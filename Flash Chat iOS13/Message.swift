db2baed9513935991e4b69f43b76746a28820ca9
Ajit Kumar Behera <nitajitrkl@gmail.com>
2021-11-23 13:48:22 +0530

    v1.02

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by ajit behera on 02/10/21.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
